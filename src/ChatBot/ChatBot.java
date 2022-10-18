11af137d2828078a7686a5a1f0045917858ee219
SHEVCHENKOKIRILL <k.o.shevchenko@student.khai.edu>
2022-10-18 20:35:32 +0300

    ChatBot 2

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,2 +1,13 @@
-package ChatBot;public class ChatBot {
+package ChatBot;
+import java.util.Scanner;
+public class ChatBot {
+    public static void main(String[] args){
+//        ChatBot 1
+        System.out.println("Hello! My name is Java_bot\nI was created in 2022");
+//        ChatBot 2
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please, remind me your name.");
+        String name = scan.nextLine();
+        System.out.println("What a great name you have, " + name);
+    }
 }
