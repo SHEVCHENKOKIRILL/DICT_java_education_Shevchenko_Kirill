9b99cdd7cfd29d8d05b082d188f5e02fa3f822db
SHEVCHENKOKIRILL <k.o.shevchenko@student.khai.edu>
2022-11-03 11:30:01 +0300

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,12 @@
 public class Test {
     public static void main(String[] args) {
- HEAD
+ 
         System.out.println("Java education");
 
-        System.out.println("education");
+       
         //new string
-HEAD
- fa3ff87 (Test.java added to feature_branch with change on string #2)
 
         System.out.println("DICT");
- ab5e13e (Test.java added to feature_branch with change on string #3)
+  
     }
 }
